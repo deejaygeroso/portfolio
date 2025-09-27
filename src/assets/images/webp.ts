1e413ec43dac916031797dc224671b8dfc33408d
Deejay Geroso <Deejay.Geroso@medirecords.com>
2025-09-27 12:13:41 +0800

    Fixed linting error (#6)
    * Fixed linting
    
    * Upgrade version

--- src/assets/images/webp.ts
+++ src/assets/images/webp.ts
@@ -1,48 +1,47 @@
-/* eslint-disable sort-imports */
 // Persons
-import angelaWebP from '@/assets/images/persons/angela-doloricon.webp'
-import aryenWebP from '@/assets/images/persons/aryen-jim-bejagan.webp'
-import becPriceWebP from '@/assets/images/persons/bec-price.webp'
-import brianAlcuazWebP from '@/assets/images/persons/brian-alcuaz.webp'
-import junskiWebP from '@/assets/images/persons/casan-jr-mala.webp'
-import chrisWebP from '@/assets/images/persons/christopher-bradshaw.webp'
-import deejay250x250WebP from '@/assets/images/persons/deejay-geroso-250x250.webp'
-import deejayWebP from '@/assets/images/persons/deejay-geroso.webp'
-import denmark250x250WebP from '@/assets/images/persons/denmark-tabiolo-250x250.webp'
-import denmarkWebP from '@/assets/images/persons/denmark-tabiolo.webp'
-import dustin250x250WebP from '@/assets/images/persons/dustin-watkins-250x250.webp'
-import dustinWebP from '@/assets/images/persons/dustin-watkins.webp'
-import faithHallWebP from '@/assets/images/persons/faith-hall.webp'
-import giovanniWebP from '@/assets/images/persons/giovanni-lobitos.webp'
-import hazelWebP from '@/assets/images/persons/hazel-ko-lumactod.webp'
-import jalilWebP from '@/assets/images/persons/jalil-laguindab.webp'
-import jasonWebP from '@/assets/images/persons/jason-abella.webp'
-import jdWebP from '@/assets/images/persons/jd.webp'
-import erickWebP from '@/assets/images/persons/john-erick-madduma.webp'
-import jonCesarWebP from '@/assets/images/persons/jon-cesar-tagsip.webp'
-import justinWebP from '@/assets/images/persons/justin-behnke.webp'
-import marcoPapsWebP from '@/assets/images/persons/marco-budiongan.webp'
-import markLalorWebP from '@/assets/images/persons/mark-lalor.webp'
-import portiaWebP from '@/assets/images/persons/portia-linao.webp'
-import richardWebP from '@/assets/images/persons/richard-paul-salem.webp'
-import papaRodsWebP from '@/assets/images/persons/roderick-gohetia.webp'
-import saraJavan250x250WebP from '@/assets/images/persons/sara-javan-250x250.webp'
-import saraJavanWebP from '@/assets/images/persons/sara-javan.webp'
-import sherylWebP from '@/assets/images/persons/sheryl-picante.webp'
-import vanessa250x250WebP from '@/assets/images/persons/vanessa-faith-abordo-250x250.webp'
-import vanessaWebP from '@/assets/images/persons/vanessa-faith-abordo.webp'
+import angelaWebP from '@/assets/images/persons/angela-doloricon.webp';
+import aryenWebP from '@/assets/images/persons/aryen-jim-bejagan.webp';
+import becPriceWebP from '@/assets/images/persons/bec-price.webp';
+import brianAlcuazWebP from '@/assets/images/persons/brian-alcuaz.webp';
+import junskiWebP from '@/assets/images/persons/casan-jr-mala.webp';
+import chrisWebP from '@/assets/images/persons/christopher-bradshaw.webp';
+import deejay250x250WebP from '@/assets/images/persons/deejay-geroso-250x250.webp';
+import deejayWebP from '@/assets/images/persons/deejay-geroso.webp';
+import denmark250x250WebP from '@/assets/images/persons/denmark-tabiolo-250x250.webp';
+import denmarkWebP from '@/assets/images/persons/denmark-tabiolo.webp';
+import dustin250x250WebP from '@/assets/images/persons/dustin-watkins-250x250.webp';
+import dustinWebP from '@/assets/images/persons/dustin-watkins.webp';
+import faithHallWebP from '@/assets/images/persons/faith-hall.webp';
+import giovanniWebP from '@/assets/images/persons/giovanni-lobitos.webp';
+import hazelWebP from '@/assets/images/persons/hazel-ko-lumactod.webp';
+import jalilWebP from '@/assets/images/persons/jalil-laguindab.webp';
+import jasonWebP from '@/assets/images/persons/jason-abella.webp';
+import jdWebP from '@/assets/images/persons/jd.webp';
+import erickWebP from '@/assets/images/persons/john-erick-madduma.webp';
+import jonCesarWebP from '@/assets/images/persons/jon-cesar-tagsip.webp';
+import justinWebP from '@/assets/images/persons/justin-behnke.webp';
+import marcoPapsWebP from '@/assets/images/persons/marco-budiongan.webp';
+import markLalorWebP from '@/assets/images/persons/mark-lalor.webp';
+import portiaWebP from '@/assets/images/persons/portia-linao.webp';
+import richardWebP from '@/assets/images/persons/richard-paul-salem.webp';
+import papaRodsWebP from '@/assets/images/persons/roderick-gohetia.webp';
+import saraJavan250x250WebP from '@/assets/images/persons/sara-javan-250x250.webp';
+import saraJavanWebP from '@/assets/images/persons/sara-javan.webp';
+import sherylWebP from '@/assets/images/persons/sheryl-picante.webp';
+import vanessa250x250WebP from '@/assets/images/persons/vanessa-faith-abordo-250x250.webp';
+import vanessaWebP from '@/assets/images/persons/vanessa-faith-abordo.webp';
 // Projects Images
-import adally1WebP from '@/assets/images/projects/adally/adally-1.webp'
-import adally2WebP from '@/assets/images/projects/adally/adally-2.webp'
-import c2cToolbarWebP from '@/assets/images/projects/clinictocloud/c2c-toolbar.webp'
-import crypto3WebP from '@/assets/images/projects/crypto/other_stats.webp'
-import crypto2WebP from '@/assets/images/projects/crypto/profitability.webp'
-import crypto1WebP from '@/assets/images/projects/crypto/volume_overview.webp'
-import databasedWebP from '@/assets/images/projects/databased/databased.webp'
-import dejablogWebP from '@/assets/images/projects/dejablog/dejablog.webp'
-import helpbertWebP from '@/assets/images/projects/helpbert/helpbert.webp'
-import meegleWebP from '@/assets/images/projects/meegle/meegle.webp'
-import monitoringWebP from '@/assets/images/projects/monitoring/monitoring.webp'
+import adally1WebP from '@/assets/images/projects/adally/adally-1.webp';
+import adally2WebP from '@/assets/images/projects/adally/adally-2.webp';
+import c2cToolbarWebP from '@/assets/images/projects/clinictocloud/c2c-toolbar.webp';
+import crypto3WebP from '@/assets/images/projects/crypto/other_stats.webp';
+import crypto2WebP from '@/assets/images/projects/crypto/profitability.webp';
+import crypto1WebP from '@/assets/images/projects/crypto/volume_overview.webp';
+import databasedWebP from '@/assets/images/projects/databased/databased.webp';
+import dejablogWebP from '@/assets/images/projects/dejablog/dejablog.webp';
+import helpbertWebP from '@/assets/images/projects/helpbert/helpbert.webp';
+import meegleWebP from '@/assets/images/projects/meegle/meegle.webp';
+import monitoringWebP from '@/assets/images/projects/monitoring/monitoring.webp';
 
 export {
   adally1WebP,
@@ -87,4 +86,4 @@ export {
   sherylWebP,
   vanessaWebP,
   vanessa250x250WebP,
-}
+};
