a3a0f4920bddab188b1adacb1982b4c45df87531
Deejay Geroso <deejaygeroso@gmail.com>
2026-03-29 08:22:14 +0800

    Fix format

--- src/data/projects/index.ts
+++ src/data/projects/index.ts
@@ -1,11 +1,11 @@
 import adally from './adally';
 import clinictocloud from './clinictocloud';
 import databased from './databased';
+import dejablog from './dejablog';
 import helpbert from './helpbert';
 import medirecords from './medirecords';
 import meegle from './meegle';
 import monitoring from './monitoring';
-import dejablog from './dejablog';
 
 // ordered by project dates;
 const projects = [medirecords, clinictocloud, adally, databased, meegle, monitoring, helpbert, dejablog];
