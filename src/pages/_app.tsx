eff89584212014db0f5ff2bc83351e3978e2d58e
Deejay Geroso <deejaygeroso@gmail.com>
2026-03-29 14:01:48 +0800

    Move theme file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,11 @@ import type { AppProps } from 'next/app';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 
+import theme from '@/styles/theme';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
-import theme from './theme';
 
 export default function MyApp(props: Readonly<AppProps>) {
   const { Component, pageProps } = props;
