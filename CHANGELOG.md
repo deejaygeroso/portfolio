0e952f0b342a05f70bb9af926c321828cbe1a8ff
Deejay Geroso <deejaygeroso@gmail.com>
2026-03-29 08:12:08 +0800

    Fix: Roboto font error in local development (#20)
    * Fix roboto font error
    
    * Add changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # portfolio
 
+## 3.1.1
+
+### Patch Changes
+
+- Fix roboto font error in local development
+
 ## 3.1.0
 
 ### Minor Changes
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,25 @@
 import type { AppProps } from 'next/app';
-import { Roboto } from 'next/font/google';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
 });
 
 export default function MyApp(props: Readonly<AppProps>) {
   const { Component, pageProps } = props;
 
   return (
-    <main className={roboto.className}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Component {...pageProps} />
-    </main>
+    </ThemeProvider>
   );
 }
