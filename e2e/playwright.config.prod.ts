13e17510fe7e488978facce84773c922783bfb03
Deejay Geroso <deejaygeroso@gmail.com>
2026-03-30 23:16:18 +0800

    chore: add Playwright configs for feature and prod environments

---
+++ e2e/playwright.config.prod.ts
@@ -0,0 +1,20 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 2,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', { open: 'never' }]],
+  use: {
+    baseURL: 'https://deejaygeroso.com',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
